5bcd3c3cd064d2ff3fa5d96f159979255bb328b3
Anders Syvertsen <andesyv@gmail.com>
2021-08-16 15:19:54 +0200

    Implemented input interaction events for main menu

--- MethodPatcher.cs
+++ MethodPatcher.cs
@@ -1,7 +1,9 @@
 ﻿
 using System;
 using ABI_RC.Core.InteractionSystem;
+using ABI_RC.Core.Savior;
 using HarmonyLib;
+using UnityEngine;
 
 namespace StickyMenu
 {
@@ -9,6 +11,11 @@ namespace StickyMenu
     {
         public static Action OnMenuEnabled;
         public static Action OnMenuDisabled;
+        public static Action OnMenuMouseDown;
+        public static Action OnMenuMouseUp;
+        public static bool MouseDownOnMenu = false;
+        public static ControllerRay rayInstance = null;
+        public static RaycastHit hitInfo = new RaycastHit();
 
         public static void DoPatching()
         {
@@ -26,6 +33,63 @@ namespace StickyMenu
                 MethodPatcher.OnMenuEnabled();
             else
                 MethodPatcher.OnMenuDisabled();
+        }
+    }
+
+    [HarmonyPatch(typeof(ControllerRay), "Update")]
+    public class ControllerRayPatch
+    {
+        private static bool LastMenuHit = false;
+        private static bool LastMouseDown = false;
+        private static bool LastMouseUp = false;
+
+        private static AccessTools.FieldRef<ControllerRay, bool> handRef =
+            AccessTools.FieldRefAccess<ControllerRay, bool>("hand");
+
+        public static void Postfix(ControllerRay __instance)
+        {
+            MethodPatcher.rayInstance = __instance;
+            var MenuHit = ViewManager.Instance.uiCollider.Raycast(
+                new Ray(__instance.transform.position, __instance.transform.TransformDirection(__instance.RayDirection)), out MethodPatcher.hitInfo,
+                1000f);
+            var FocusAwayFromMenu = LastMenuHit && !MenuHit;
+            LastMenuHit = MenuHit;
+
+            var down = MenuHit && MouseDown(__instance);
+            var up = FocusAwayFromMenu || (MenuHit && MouseUp(__instance));
+
+            var pressed = !LastMouseDown && down;
+            var released = LastMouseDown && !LastMouseUp && up;
+
+            LastMouseDown = down;
+            LastMouseUp = up;
+
+            if (pressed)
+            {
+                MethodPatcher.MouseDownOnMenu = true;
+                MethodPatcher.OnMenuMouseDown();
+            }
+
+            if (released)
+            {
+                MethodPatcher.MouseDownOnMenu = false;
+                MethodPatcher.OnMenuMouseUp();
+            }
+        }
+
+        private static bool MouseDown(ControllerRay __instance)
+        {
+            var hand = handRef(__instance);
+            return (hand ? CVRInputManager.Instance.interactLeftDown : CVRInputManager.Instance.interactRightDown) ||
+                   ((double)(hand ? CVRInputManager.Instance.interactLeftValue : CVRInputManager.Instance.interactRightValue) > 0.800000011920929) ||
+                   (hand ? CVRInputManager.Instance.gripLeftDown : CVRInputManager.Instance.gripRightDown);
+        }
+
+        private static bool MouseUp(ControllerRay __instance)
+        {
+            var hand = handRef(__instance);
+            return (hand ? CVRInputManager.Instance.interactLeftUp : CVRInputManager.Instance.interactRightUp) ||
+                   (hand ? CVRInputManager.Instance.gripLeftUp : CVRInputManager.Instance.gripRightUp);
         }
     }
 }
