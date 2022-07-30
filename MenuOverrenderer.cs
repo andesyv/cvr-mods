3db8100ab407166c036e5bde38130bff9cb14345
Anders Syvertsen <andesyv@gmail.com>
2022-07-30 13:25:42 +0200

    Inital commit (MenuOverrenderer)

---
+++ MenuOverrenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MenuOverrenderer
+{
+    [BepInPlugin("dev.syvertsen.plugins.menuoverrenderer", "MenuOverrenderer", "0.0.1")]
+    [BepInProcess("ChilloutVR.exe")]
+    public class MenuOverrenderer : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("Hello world!");
+        }
+
+        private void OnDestroy()
+        {
+            Logger.LogInfo("Goodbye world!");
+        }
+    }
+}
