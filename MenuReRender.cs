88d3d9715b94e5849b368019c8aa539036d13228
Anders Syvertsen <andesyv@gmail.com>
2022-07-31 20:19:21 +0200

    Refactor and renaming

---
+++ MenuReRender.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using ABI_RC.Core.InteractionSystem;
+using BepInEx;
+using cohtml;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MenuReRender
+{
+    [BepInPlugin("dev.syvertsen.cvr.plugins.menurerender", "MenuReRender", "0.0.1")]
+    [BepInProcess("ChilloutVR.exe")]
+    public class MenuReRender : BaseUnityPlugin
+    {
+        private const string MainMenuViewName = "CohtmlWorldView";
+        private GameObject _mainMenu;
+        private RenderReplacer _replacerInstance;
+        private AssetBundle _assets;
+        private static Material _uiMaterial;
+        private Action<bool> _onMenuToggle;
+        private Harmony _harmonyInstance;
+        private static MenuReRender _instance;
+
+        private void Awake()
+        {
+            _instance = this;
+            if (!Init())
+                SceneManager.sceneLoaded += OnSceneWasLoaded;
+        }
+
+        public void OnSceneWasLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (Init())
+                SceneManager.sceneLoaded -= OnSceneWasLoaded;
+        }
+
+        private bool Init()
+        {
+            _mainMenu = FindMainMenu();
+            if (!_mainMenu)
+                return false;
+
+            if (!LoadAssets())
+                return false;
+
+            // Setup RenderReplacer components
+            _replacerInstance = _mainMenu.AddComponent<RenderReplacer>();
+            if (!_replacerInstance)
+                return false;
+
+            _replacerInstance.animator = ViewManager.Instance.uiMenuAnimator;
+            _replacerInstance.material = _uiMaterial;
+            _onMenuToggle += (isEnabled) => _replacerInstance.enabled = isEnabled;
+
+            // Patch functions
+            try
+            {
+                _harmonyInstance =
+                    Harmony.CreateAndPatchAll(typeof(MenuReRender), "dev.syvertsen.cvr.plugins.menurerender");
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+                return false;
+            }
+
+            Logger.LogInfo("MenuOverRenderer done initializing!");
+            return true;
+        }
+
+        private void OnDestroy()
+        {
+            Logger.LogDebug("Running cleanup!");
+
+            _harmonyInstance?.UnpatchSelf();
+
+            if (_replacerInstance)
+                Destroy(_replacerInstance);
+
+            if (_uiMaterial)
+                Destroy(_uiMaterial);
+            if (_assets)
+                _assets.Unload(true);
+        }
+
+        private static GameObject FindMainMenu()
+        {
+            var objects = FindObjectsOfType<CohtmlView>();
+            return objects.FirstOrDefault(view =>
+                    string.Equals(view.gameObject.name, MainMenuViewName, StringComparison.OrdinalIgnoreCase))
+                ?.gameObject;
+        }
+
+        private bool LoadAssets()
+        {
+            Logger.LogDebug("Loading assets");
+            var stream = Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream("MenuReRender.Resources.Assets.Bundle.menurerender.bundle");
+            if (stream == null)
+            {
+                Logger.LogError("Failed to fetch reasource stream from assembly!");
+                return false;
+            }
+
+            _assets = AssetBundle.LoadFromStream(stream);
+            if (!_assets)
+            {
+                Logger.LogError("Failed to load assetbundle!");
+                return false;
+            }
+
+            _assets.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+
+            _uiMaterial = _assets.LoadAsset<Material>("ui.mat");
+            if (!_uiMaterial)
+            {
+                Logger.LogError("Failed to fetch material from assetbundle!");
+                return false;
+            }
+
+            _uiMaterial.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+            _uiMaterial.EnableKeyword("UNITY_UV_STARTS_AT_TOP");
+
+            Logger.LogDebug("Finished loading assets");
+
+            return true;
+        }
+
+        [HarmonyPatch(typeof(ViewManager), nameof(ViewManager.UiStateToggle), typeof(bool))]
+        [HarmonyPostfix]
+        private static void UiTogglePatch(ViewManager __instance, bool show)
+        {
+            _instance._onMenuToggle?.Invoke(show);
+        }
+    }
+}
---
+++ RenderReplacer.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BepInEx.Logging;
+using cohtml;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace MenuReRender
+{
+    internal class RenderReplacer : MonoBehaviour
+    {
+        private Renderer[] _renderers;
+        private bool[] _enabledStates;
+        private readonly Dictionary<Camera, CommandBuffer> _cameras = new();
+        public Material material;
+        private RenderTexture _menuRenderTexture;
+        private ManualLogSource Logger { get; } = BepInEx.Logging.Logger.CreateLogSource("RenderReplacer");
+        public Animator animator;
+
+        private void Awake()
+        {
+            _renderers = GetComponentsInChildren<Renderer>();
+            _enabledStates = _renderers.Select(r => r.enabled).ToArray();
+            var view = GetComponent<CohtmlView>();
+            if (!view)
+                Logger.LogError("View was not found!");
+            else
+                _menuRenderTexture = view.ViewTexture;
+        }
+
+        private bool IsVisible()
+        {
+            if (!animator)
+                return true;
+            var clips = animator.GetCurrentAnimatorClipInfo(0);
+            return 0 < clips.Count(c => c.clip.name is "Open");
+        }
+
+        // Inspiration for using command buffers taken from
+        // https://forum.unity.com/threads/rendering-an-object-multiple-times-with-different-materials.505138/
+        private void OnWillRenderObject()
+        {
+            var cam = Camera.current;
+            if (_cameras.ContainsKey(cam))
+            {
+                // Something keeps re-enabling the renderers each frame (probably with good reason),
+                // but we have to disable em so we don't render twice
+                foreach (var rend in _renderers)
+                    rend.enabled = false;
+
+                return;
+            }
+
+            if (!IsVisible())
+                return;
+            
+            // Build command buffer
+            var cb = new CommandBuffer();
+            // var shaderID = Shader.PropertyToID("_MenuOverlay");
+            // cb.GetTemporaryRT(shaderID, -1, -1, 16, FilterMode.Bilinear);
+            // cb.SetRenderTarget(shaderID);
+            // cb.ClearRenderTarget(true, true, Color.cyan, 0.0f);
+            cb.SetRenderTarget(BuiltinRenderTextureType.CameraTarget);
+
+            foreach (var rend in _renderers)
+            {
+                if (_menuRenderTexture)
+                    cb.SetGlobalTexture("_CurrentUITexture", _menuRenderTexture);
+
+                cb.DrawRenderer(rend, material);
+                rend.enabled = false;
+            }
+
+            // cb.SetRenderTarget(BuiltinRenderTextureType.CameraTarget);
+            // cb.Blit(shaderID, BuiltinRenderTextureType.CameraTarget);
+            // cb.ReleaseTemporaryRT(shaderID);
+
+            // Add command buffer
+            cam.AddCommandBuffer(CameraEvent.AfterEverything, cb);
+            _cameras[cam] = cb;
+            
+            Logger.LogDebug("Added command buffer!");
+        }
+
+        private void OnDisable()
+        {
+            foreach (var (cam, cb) in _cameras)
+                cam.RemoveCommandBuffer(CameraEvent.AfterEverything, cb);
+            _cameras.Clear();
+
+            _renderers.Zip(_enabledStates, (rend, enable) => rend.enabled = enable);
+            Logger.LogDebug("Removed command buffer!");
+        }
+    }
+}
