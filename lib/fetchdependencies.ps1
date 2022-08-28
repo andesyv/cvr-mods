1688860b9c9ea51b0d2bdc843aec9a92fc30d4ce
Anders Syvertsen <andesyv@gmail.com>
2022-08-28 22:42:53 +0200

    New mod!

---
+++ lib/fetchdependencies.ps1
@@ -0,0 +1,4 @@
+# I have no fancy way to autodownload manual individual git releases, so this is what we get
+# https://git.ljoonal.xyz/ljoonal/CVR-Plugins/releases/tag/v2022-08-01
+curl "https://git.ljoonal.xyz/attachments/e832853c-c26f-4cc4-9fb3-b004fbea88c7" -O HopLib.dll
+curl "https://git.ljoonal.xyz/attachments/082da30f-c33e-4c39-bdc7-3ca44fb85627" -O HopLib.xml
---
+++ src/AutoReConnect/AutoReConnect.cs
@@ -0,0 +1,132 @@
+﻿using ABI_RC.Core.InteractionSystem;
+using ABI_RC.Core.Networking;
+using ABI_RC.Core.Networking.API.Responses;
+using ABI_RC.Core.Networking.API;
+using ABI_RC.Core.Networking.IO.Instancing;
+using BepInEx;
+using BepInEx.Unity.Mono;
+using HarmonyLib;
+using System.Collections;
+using System.Threading.Tasks;
+using HopLib;
+using System;
+using ABI_RC.Core.Savior;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AutoReConnect;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("ChilloutVR.exe")]
+[BepInDependency("xyz.ljoonal.cvr.hoplib", "0.1.0")]
+public class AutoReConnect : BaseUnityPlugin
+{
+    private Dictionary<string, bool> _coroutineRunning = new Dictionary<string, bool>();
+    private static InstanceDetails? _instanceDetails = null;
+    private bool _isJoining = false;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+
+        HopApi.InstanceJoined += delegate
+        {
+            _isJoining = false;
+            StartOrRestart("FetchInstanceDetailsCoroutine");
+        };
+
+        HopApi.InstanceJoiningStarted += delegate
+        {
+            _isJoining = true;
+        };
+
+        HopApi.InstanceDisconnect += delegate
+        {
+            if (_coroutineRunning["FetchInstanceDetailsCoroutine"])
+                Stop("FetchInstanceDetailsCoroutine");
+
+            StartOrRestart("TryReconnectCoroutine");
+        };
+    }
+
+    public struct InstanceDetails
+    {
+        public string instanceId;
+        public string worldId;
+        public long timestamp;
+
+        public static bool Equals(InstanceDetails lhs, InstanceDetails rhs) =>
+            lhs.instanceId == rhs.instanceId && lhs.worldId == rhs.worldId && lhs.timestamp != rhs.timestamp;
+        public static bool Equals(InstanceDetails? lhs, InstanceDetails? rhs) =>
+            lhs != null && rhs != null && Equals(lhs ?? new InstanceDetails { }, rhs ?? new InstanceDetails { });
+    }
+
+    private void StartOrRestart(string coroutine)
+    {
+        if (_coroutineRunning[coroutine])
+            StopCoroutine(coroutine);
+        StartCoroutine(coroutine);
+    }
+
+    private void Stop(string coroutine)
+    {
+        if (_coroutineRunning[coroutine])
+            StopCoroutine(coroutine);
+        _coroutineRunning[coroutine] = false;
+    }
+
+    private IEnumerator FetchInstanceDetailsCoroutine()
+    {
+        _coroutineRunning["FetchInstanceDetailsCoroutine"] = true;
+        var task = FetchCurrentInstanceDetails();
+        while (!task.IsCompleted)
+            yield return null;
+        _instanceDetails = task.Result;
+        Logger.LogDebug($"Current instance is ${_instanceDetails?.instanceId ?? "unknown"}");
+        _coroutineRunning["FetchInstanceDetailsCoroutine"] = false;
+    }
+
+    private IEnumerator TryReconnectCoroutine()
+    {
+        var cachedInstanceDetails = _instanceDetails;
+        _coroutineRunning["TryReconnectCoroutine"] = true;
+        yield return new WaitForSeconds(5f);
+
+        // If we're currently not joining an instance and hasn't changed instance since last time, we will try to reconnect
+        if (!_isJoining && cachedInstanceDetails != null && InstanceDetails.Equals(cachedInstanceDetails, _instanceDetails))
+            Instances.SetJoinTarget(cachedInstanceDetails.Value.instanceId, cachedInstanceDetails.Value.worldId);
+
+        _coroutineRunning["TryReconnectCoroutine"] = false;
+    }
+
+    private async Task<InstanceDetails?> FetchCurrentInstanceDetails()
+    {
+        return !string.IsNullOrEmpty(MetaPort.Instance.CurrentInstanceId) ? await FetchInstanceDetails(MetaPort.Instance.CurrentInstanceId) : null;
+    }
+
+    private async Task<InstanceDetails?> FetchInstanceDetails(string InstanceId)
+    {
+        BaseResponse<InstanceDetailsResponse> baseResponse = await ApiConnection.MakeRequest<InstanceDetailsResponse>(ApiConnection.ApiOperation.InstanceDetail, new
+        {
+            instanceID = InstanceId
+        });
+        if (baseResponse == null)
+        {
+            return null;
+        }
+
+        return new InstanceDetails
+        {
+            instanceId = baseResponse.Data.Id,
+            worldId = baseResponse.Data.World.Id,
+            timestamp = CreateTimestamp()
+        };
+    }
+
+    private static long CreateTimestamp()
+    {
+        return ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
+    }
+}
