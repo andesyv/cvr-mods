5e9b44c495ffed028933b7aa52916df507ddb70b
Anders Syvertsen <anders@syvertsen.dev>
2022-10-08 00:17:18 +0200

    Unity native-plugin hello world

--- src/CameraViewers/plugin/src/main.cpp
+++ src/CameraViewers/plugin/src/main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
+#include "IUnityGraphics.h"
 
-int main() {
-  std::cout << "Hello world!" << std::endl;
-  return 0;
+#include <fstream>
+
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API RegisterPlugin()
+{
+  std::ofstream file{"pluginoutput.txt", std::ios::out | std::ios::trunc};
+  if (file)
+  {
+	  file << "Hello plugin!" << std::endl;
+  }
 }
